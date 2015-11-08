2b1c8f64f162dbace541e2cd15ab954fb8a987ac
Finalreq <arminra7@gmail.com>
2015-11-07 18:59:27 -0800

    update

--- main.c
+++ main.c
@@ -6,6 +6,22 @@ typedef struct node{
 	struct node* next;
 }Node;
 
+int gethash (int n){
+	int hash = n % 13;
+	return hash;
+}
+
+int add_node (Node *table, Node *n){
+	int hash = gethash(n->val);
+	Node* tmp = &table[hash];
+	while (tmp->next != NULL){
+		tmp = tmp->next;
+	}
+	tmp->next = n;
+	return 1;
+}
+
+
 int main(){
 	int i = 0;
 	Node *hash = (Node*)malloc (sizeof (Node)*13);
@@ -14,9 +30,14 @@ int main(){
 		hash[i].next = (Node*) malloc (sizeof (Node));
 	}
 	hash[1].next->val = 17;
-
-	printf ("%d, %d \n", hash[1].val, hash[1].next->val);
-	
+	Node *n = malloc (sizeof(Node));
+	n->val = 14;
+	n->next = NULL;
+	add_node (hash, n);
+	printf ("%d, %d, %d \n", hash[1].val, hash[1].next->val, hash[1].next->next->val);
+	if (hash[5].next == NULL){
+		printf ("ha\n");
+	}
 	printf ("Hello World! %d\n", i);
 	return 0;
 }
